0f500ab94e9b3968aa5b07c0c082e2fbbf9d1d6a
Chris <kristina.beidina@gmail.com>
2021-05-29 23:43:01 +0300

    edit python file

--- inferring_result.py
+++ inferring_result.py
@@ -6,7 +6,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
-    def __init__(self):
         super(Net, self).__init__()                
         self.conv1 = nn.Conv2d(1, 3, 3)
         self.pool = nn.MaxPool2d(2, 2)
